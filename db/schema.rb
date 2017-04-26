4c295040cf3ba24f47876abb279d4b748b23b150
Keith Thompson <me@keiththomps.com>
2017-04-25 20:01:30 -0400

    Add Ingredient model to demonstrate deployment of update
    This commit adds an additional model to the application for the
    purpose of showing how we would go about deploying an application
    update to our app that is running in a Kubernetes cluster. We address
    the application update portion and also the database migration portion.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205100045) do
+ActiveRecord::Schema.define(version: 20170425120709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meal_plans", force: :cascade do |t|
     t.date     "start_date", null: false
---
+++ test/factories/ingredients.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :ingredient do
+    name "MyString"
+  end
+end
