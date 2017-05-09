5c0679f493b3eb2cff01cba8c8ff074371cb1efb
Keith Thompson <me@keiththomps.com>
2017-05-09 17:35:12 -0400

    Upgrade to Rails 5.1.0
    This go through the changes necessary to take the existing Rails
    5.0.0.1 application up to Rails 5.1.0. We're not yet utilizing these
    new features, but we might shortly.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '~> 5.1.0'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -15,50 +15,50 @@ ActiveRecord::Schema.define(version: 20170425120709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ingredients", force: :cascade do |t|
-    t.string   "name"
+  create_table "ingredients", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "meal_plans", force: :cascade do |t|
-    t.date     "start_date", null: false
-    t.date     "end_date",   null: false
-    t.integer  "user_id",    null: false
+  create_table "meal_plans", id: :serial, force: :cascade do |t|
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_meal_plans_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_meal_plans_on_user_id"
   end
 
-  create_table "meals", force: :cascade do |t|
-    t.date     "date",         null: false
-    t.integer  "meal_plan_id", null: false
-    t.integer  "recipe_id",    null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["meal_plan_id"], name: "index_meals_on_meal_plan_id", using: :btree
-    t.index ["recipe_id"], name: "index_meals_on_recipe_id", using: :btree
+  create_table "meals", id: :serial, force: :cascade do |t|
+    t.date "date", null: false
+    t.integer "meal_plan_id", null: false
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meal_plan_id"], name: "index_meals_on_meal_plan_id"
+    t.index ["recipe_id"], name: "index_meals_on_recipe_id"
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.text     "description", null: false
-    t.integer  "user_id",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id", "name"], name: "index_recipes_on_user_id_and_name", using: :btree
-    t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
+  create_table "recipes", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "name"], name: "index_recipes_on_user_id_and_name"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
-    t.index ["email"], name: "index_users_on_email", using: :btree
-    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "meal_plans", "users"
